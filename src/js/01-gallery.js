9726f20548ce00cc85fed38b58fb5115e76aafc2
DanyloYamelynets <130572960+DanyloYamelynets@users.noreply.github.com>
2023-06-10 19:31:56 +0300

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,43 @@
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
-console.log(SimpleLightbox);
+const myGallery = document.querySelector('.gallery');
+
+const galleryList = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+  })
+  .join('');
+
+myGallery.insertAdjacentHTML('beforeend', galleryList);
+myGallery.addEventListener('click', onImageClick);
+
+function onImageClick(event) {
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+  event.preventDefault();
+  const instance = SimpleLightbox.create(`
+	<img src="${event.target.dataset.source}">
+`);
+  instance.show();
+
+  myGallery.addEventListener('keydown', event => {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  });
+}
+
