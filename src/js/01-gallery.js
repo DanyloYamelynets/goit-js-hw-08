26580a774cb221f9bace36e0c41f9c0752194a7c
DanyloYamelynets <130572960+DanyloYamelynets@users.noreply.github.com>
2023-06-11 13:14:25 +0300

    01 task done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -22,22 +22,7 @@ const galleryList = galleryItems
   .join('');
 
 myGallery.insertAdjacentHTML('beforeend', galleryList);
-myGallery.addEventListener('click', onImageClick);
-
-function onImageClick(event) {
-  if (!event.target.classList.contains('gallery__image')) {
-    return;
-  }
-  event.preventDefault();
-  const instance = SimpleLightbox.create(`
-	<img src="${event.target.dataset.source}">
-`);
-  instance.show();
-
-  myGallery.addEventListener('keydown', event => {
-    if (event.code === 'Escape') {
-      instance.close();
-    }
-  });
-}
-
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
