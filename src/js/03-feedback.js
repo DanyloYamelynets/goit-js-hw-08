d60dc7b64cb6e13da0cf35e7120011064988a299
DanyloYamelynets <130572960+DanyloYamelynets@users.noreply.github.com>
2023-06-13 21:09:32 +0300

    task 3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,2 +1,33 @@
 import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const formEmail = document.querySelector('input');
+const formMessage = document.querySelector('textarea');
+const storageKey = 'feedback-form-state';
+
+const throttledStorage = throttle(() => {
+  const formData = {
+    email: formEmail.value,
+    message: formMessage.value,
+  };
+  localStorage.setItem(storageKey, JSON.stringify(formData));
+}, 500);
+feedbackForm.addEventListener('input', evt => {
+  throttledStorage();
+});
+
+function afterReload() {
+  const dataStorage = localStorage.getItem(storageKey);
+  if (dataStorage) {
+    const formData = JSON.parse(dataStorage);
+    formEmail.value = formData.email;
+    formMessage.value = formData.message;
+  }
+}
+afterReload();
+
+feedbackForm.addEventListener('submit', evt => {
+  evt.preventDefault();
+  evt.target.reset();
+  localStorage.removeItem(storageKey);
+});
